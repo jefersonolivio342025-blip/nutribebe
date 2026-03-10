75edb4d62ed085acb421d113256f1fc4a0a44aa7
jefersonolivio342025-blip <jefersonolivio342025@gmail.com>
2026-03-10 19:01:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Esta linha resolve a tela branca em domínios personalizados
   server: {
     host: "::",
     port: 8080,
